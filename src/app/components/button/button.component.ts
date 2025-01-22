64a7417675a4b5e21d0d7e1d1af1899e64223bf3
sueecabral <suee.scabral@gmail.com>
2025-01-22 20:32:02 -0300

    create button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,8 +1,9 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
