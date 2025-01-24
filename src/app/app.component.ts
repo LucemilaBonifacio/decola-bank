893eafdaed67b16cf05a361c1797765940f26681
Lucemila Bonifácio <lucemaylabb@gmail.com>
2025-01-23 23:01:00 -0300

    Create component card-app-home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardAppHomeComponent } from "./components/card-app-home/card-app-home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardAppHomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
