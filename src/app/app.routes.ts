c419ee5a87092e30571d418f497d47001087a0f1
Tamyres Pereira da Silva <t.pereira.da.silva@avanade.com>
2025-01-26 19:59:10 -0300

    atualização banner home e componente abrir conta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginClienteComponent } from './components/cliente/login-cliente/login-cliente.component';
-
-
+import { FormAbrirContaComponent } from './components/form-abrir-conta/form-abrir-conta.component';
 
 
 export const routes: Routes = [
 {path:"", redirectTo:"home", pathMatch:"full"},
 {path: 'home', component: HomeComponent},
+{path: 'abra-sua-conta', component: FormAbrirContaComponent},
 {path: 'login/cliente', component: LoginClienteComponent}
-
 
 ];
