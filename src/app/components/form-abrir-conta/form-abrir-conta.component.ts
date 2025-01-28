fcda6cfcda07bf082230067807676f0e2d5efc23
Tamyres Pereira da Silva <t.pereira.da.silva@avanade.com>
2025-01-28 14:48:51 -0300

    update form

--- src/app/components/form-abrir-conta/form-abrir-conta.component.ts
+++ src/app/components/form-abrir-conta/form-abrir-conta.component.ts
@@ -1,47 +1,32 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { FooterComponent } from "../footer/footer.component";
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-abrir-conta',
-  imports: [CommonModule, ReactiveFormsModule, FooterComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './form-abrir-conta.component.html',
   styleUrls: ['./form-abrir-conta.component.css']
 })
 export class FormAbrirContaComponent {
-  form: FormGroup;
+  cpf: string = '';
+  nomeCompleto: string = '';
+  email: string = '';
+  confirmacaoEmail: string = '';
+  politica: boolean = false;
   mostrarPopup = false;
 
-  constructor(private fb: FormBuilder, private router: Router) {
-    this.form = this.fb.group({
-      cpf: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
-      nomeCompleto: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      confirmacaoEmail: ['', Validators.required],
-      politica: [false, Validators.requiredTrue]
-    }, { validator: this.emailMatchValidator });
-  }
-
-  emailMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
-    const email = control.get('email');
-    const confirmacaoEmail = control.get('confirmacaoEmail');
-    if (email?.value !== confirmacaoEmail?.value) {
-      return { 'emailMismatch': true };
-    }
-    return null;
-  }
+  constructor(private router: Router) {}
 
   fecharFormulario() {
     this.router.navigate(['/home']);
   }
 
-  onSubmit() {
-    if (this.form.valid) {
+  onSubmit(form: any) {
+    if (form.valid) {
       this.mostrarPopup = true;
-      console.log('Formulário enviado com sucesso:', this.form.value);
+      console.log('Formulário enviado com sucesso:', form.value);
     }
   }
 
