92cc4dd07b127a59557bf6f05aac3d023c39f239
Lucemila Bonifácio <lucemaylabb@gmail.com>
2025-01-28 20:37:17 -0300

    correção rota gerente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginClienteComponent } from './components/cliente/login-cliente/login-cliente.component';
 import { FormAbrirContaComponent } from './components/form-abrir-conta/form-abrir-conta.component';
 import { TelaInicialClienteComponent } from './components/cliente/tela-inicial-cliente/tela-inicial-cliente.component';
+import { LoginGerenteComponent } from './components/gerente/login-gerente/login-gerente.component';
 
 
 
@@ -12,7 +13,8 @@ export const routes: Routes = [
 {path: 'home', component: HomeComponent},
 {path: 'abra-sua-conta', component: FormAbrirContaComponent},
 {path: 'login/cliente', component: LoginClienteComponent},
-{path: 'tela-inicial-cliente', component: TelaInicialClienteComponent}
+{path: 'tela-inicial-cliente', component: TelaInicialClienteComponent},
+{path: 'login/gerente', component: LoginGerenteComponent}
 
 
 
