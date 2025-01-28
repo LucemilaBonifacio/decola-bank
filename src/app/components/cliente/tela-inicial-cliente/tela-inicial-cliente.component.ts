561675c47daff6384d3146f136e399c067becc2a
Lucemila Bonifácio <lucemaylabb@gmail.com>
2025-01-28 20:23:24 -0300

    create tela inicial do cliente

---
+++ src/app/components/cliente/tela-inicial-cliente/tela-inicial-cliente.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-tela-inicial-cliente',
+  imports: [NgOptimizedImage, CommonModule],
+  templateUrl: './tela-inicial-cliente.component.html',
+  styleUrl: './tela-inicial-cliente.component.css'
+})
+export class TelaInicialClienteComponent {
+
+}
