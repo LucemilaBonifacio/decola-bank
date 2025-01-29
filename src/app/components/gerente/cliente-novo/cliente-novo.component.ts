de58601e54604d2d216d8675568c8543723e3c9b
Tamyres Pereira da Silva <t.pereira.da.silva@avanade.com>
2025-01-29 13:19:27 -0300

    rotas crud clientes

---
+++ src/app/components/gerente/cliente-novo/cliente-novo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-novo',
+  imports: [],
+  templateUrl: './cliente-novo.component.html',
+  styleUrl: './cliente-novo.component.css'
+})
+export class ClienteNovoComponent {
+
+}
