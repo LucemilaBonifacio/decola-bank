41b234dcc27fc12ac374516b16546308e19e2d40
Lucemila Bonifácio <lucemaylabb@gmail.com>
2025-01-29 14:23:17 -0300

    Update component pagamento

--- src/app/components/cliente/tela-inicial-cliente/pagamento/pagamento.component.ts
+++ src/app/components/cliente/tela-inicial-cliente/pagamento/pagamento.component.ts
@@ -22,6 +22,8 @@ export class PagamentoComponent {
   verificarSaldo() {
     if (this.valorPagamento > this.saldoCliente) {
       this.mensagemErro = 'Erro: O valor digitado é maior que o saldo disponível.';
+    } else if (!/^\d{1,48}$/.test(this.codigoPagamento)) {
+      this.mensagemErro = 'Erro: O código de pagamento deve conter apenas números e ter no máximo 48 dígitos.';
     } else {
       this.mensagemErro = '';
       this.saldoCliente -= this.valorPagamento;
