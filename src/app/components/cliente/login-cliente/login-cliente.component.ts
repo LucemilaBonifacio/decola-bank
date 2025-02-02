ecd1f3af8d2fdc2490514dfa1e837309c9d030cb
Lucemila Bonifácio <lucemaylabb@gmail.com>
2025-02-02 14:27:49 -0300

    create auth e guard

--- src/app/components/cliente/login-cliente/login-cliente.component.ts
+++ src/app/components/cliente/login-cliente/login-cliente.component.ts
@@ -1,56 +1,53 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { RouterLink } from '@angular/router';
-import { Conta } from '../../../classes/conta';
 import { ClienteService } from '../../../services/clientes.service';
-
+import { AuthService } from '../../../services/auth.service';
+import { Conta } from '../../../classes/conta';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-cliente',
-  imports: [CommonModule, FormsModule, RouterLink],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login-cliente.component.html',
   styleUrls: ['./login-cliente.component.css']
 })
-
 export class LoginClienteComponent {
-  conta: Conta;
+  conta: Conta = new Conta('', '', 0, new Date(), 0, 0, '', '', 0);
   senha: string = '';
-  numConta: string =''
+  numConta: string = '';
 
-  constructor(private clienteService: ClienteService) {
-    // Inicialize a conta com valores padrão
-    this.conta = new Conta('', '', 0, new Date(), 0, 0, '', '', 0);
-  }
+  constructor(
+    private clienteService: ClienteService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  validateLength(event: Event, maxLength: number): void {
-    const input = event.target as HTMLInputElement;
-    if (input.value.length > maxLength) {
-      input.value = input.value.slice(0, maxLength);
-    }
-  }
-
-  onSubmit(form: NgForm): void {
-    if (form.valid) {
-      // Processar o login
-      this.obterConta(this.conta.numConta);
+  onSubmit(): void {
+    if (this.numConta) {
+      this.fazerLogin(this.numConta);
     } else {
-      console.log('Formulário inválido');
+      console.log('Número da conta inválido');
     }
+  }
+
+  fazerLogin(numConta: string): void {
+    this.obterConta(numConta);
   }
 
   obterConta(numConta: string): void {
-    
     this.clienteService.obterConta(numConta).subscribe(
       (conta: Conta) => {
         this.conta = conta;
+        this.authService.setConta(conta);
         console.log('Dados da Conta:', this.conta);
-        sessionStorage.setItem('contaCliente', JSON.stringify(this.conta));
+        this.router.navigate(['/tela-inicial-cliente']);
       },
       (error) => {
         console.error('Erro ao obter conta:', error);
       }
     );
-   
   }
-}
+
+ }
--- src/app/components/cliente/tela-inicial-cliente/tela-inicial-cliente.component.ts
+++ src/app/components/cliente/tela-inicial-cliente/tela-inicial-cliente.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Conta } from '../../../classes/conta';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Conta } from '../../../classes/conta';
 
 @Component({
   selector: 'app-tela-inicial-cliente',
-  standalone: true,
-  imports: [NgOptimizedImage, CommonModule, RouterLink],
+  imports: [NgOptimizedImage,CommonModule, RouterLink],
   templateUrl: './tela-inicial-cliente.component.html',
   styleUrls: ['./tela-inicial-cliente.component.css']
 })
 export class TelaInicialClienteComponent implements OnInit {
-  conta: Conta = new Conta('', '', 0, new Date(), 0, 0, '', '', 0); 
+  conta: Conta = new Conta('', '', 0, new Date(), 0, 0, '', '', 0);
   saldo: number = 0;
   nomeCliente: string = '';
 
-  ngOnInit(): void {
-    const contaStorage = sessionStorage.getItem('contaCliente');
+  constructor(private authService: AuthService) {}
 
-    if (contaStorage) {
-      this.conta = JSON.parse(contaStorage) as Conta;
-      this.nomeCliente = this.conta?.nomeCliente || ''; 
-      this.saldo = this.conta?.saldo || 0; 
+  ngOnInit(): void {
+    const contaSalva = this.authService.getConta();
+
+    if (contaSalva) {
+      this.conta = contaSalva;
+      this.nomeCliente = this.conta.nomeCliente;
+      this.saldo = this.conta.saldo;
     }
   }
 }
