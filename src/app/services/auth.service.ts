aa3887e1cf67a0b5bfa2d24995bf84b7478e2553
sueecabral <suee.scabral@gmail.com>
2025-02-02 18:33:55 -0300

    Button logoff navbar

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Conta } from '../classes/conta';
 
 @Injectable({
@@ -6,20 +7,28 @@ import { Conta } from '../classes/conta';
 })
 export class AuthService {
   private chaveConta = 'contaCliente';
+  private loggedIn = new BehaviorSubject<boolean>(this.hasConta());
 
-  
-  setConta(conta: Conta): void {
-    localStorage.setItem(this.chaveConta, JSON.stringify(conta)); 
+  get isLoggedIn() {
+    return this.loggedIn.asObservable();
   }
 
-  
+  private hasConta(): boolean {
+    return !!localStorage.getItem(this.chaveConta);
+  }
+
+  setConta(conta: Conta): void {
+    localStorage.setItem(this.chaveConta, JSON.stringify(conta));
+    this.loggedIn.next(true);
+  }
+
   getConta(): Conta | null {
     const contaStorage = localStorage.getItem(this.chaveConta);
     return contaStorage ? JSON.parse(contaStorage) as Conta : null;
   }
 
-  
   logout(): void {
     localStorage.removeItem(this.chaveConta);
+    this.loggedIn.next(false);
   }
 }
